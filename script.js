121643977731353b72cce8bc712bd02bcef072b5
tamerfs <tamer.srhn@gmail.com>
2022-07-19 01:22:25 -0300

    Novas alterações

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function mudaTema(){
    document.body.classList.toggle("dark") 
+  
+   let buttonText = document.getElementById("mode");
+    (buttonText.textContent == 'Dark Mode')? buttonText.innerHTML = 'Light Mode':
+      buttonText.innerHTML = 'Dark Mode';
 }
+
+
